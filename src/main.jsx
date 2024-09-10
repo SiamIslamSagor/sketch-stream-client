be0705088aace2b4158afcb1107edabee7362013
SiamIslamSagor <msiamislam12345@gmail.com>
2024-09-11 03:54:18 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,23 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <div>
+        <h1 className="text-3xl text-blue-800 font-bold underline">
+          Hello world!
+        </h1>
+      </div>
+    ),
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
