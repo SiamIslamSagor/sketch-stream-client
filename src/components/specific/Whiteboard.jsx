2287270b776452d08eea9ef5182b7f467d827cf1
SiamIslamSagor <msiamislam12345@gmail.com>
2024-09-12 23:28:37 +0600

    added rectangle and circle drawing functionality

--- src/components/specific/Whiteboard.jsx
+++ src/components/specific/Whiteboard.jsx
@@ -1,44 +1,102 @@
-// client/src/components/Whiteboard.js
 import Konva from "konva";
 import { useRef, useState } from "react";
-import { Layer, Line, Stage } from "react-konva";
+import { Circle, Layer, Line, Rect, Stage } from "react-konva";
 
 function Whiteboard() {
   const [drawing, setDrawing] = useState(false);
   const [lines, setLines] = useState([]);
+  const [rectangles, setRectangles] = useState([]);
+  const [circles, setCircles] = useState([]);
   const canvasRef = useRef(null);
   const stageRef = useRef(null);
+  const [drawingMode, setDrawingMode] = useState("freehand");
 
   const handleMouseDown = event => {
     setDrawing(true);
-    const stage = stageRef.current;
-    const layer = stage.getLayer();
-    const pointerPosition = stage.getPointerPosition();
-    const line = new Konva.Line({
-      points: [pointerPosition.x, pointerPosition.y],
-      stroke: "black",
-      strokeWidth: 2,
-      lineCap: "round",
-      lineJoin: "round",
-    });
-    layer?.add(line);
-    setLines([...lines, line]);
+    if (drawingMode === "rectangle") {
+      const stage = stageRef.current;
+      const layer = stage.getLayer();
+      const pointerPosition = stage.getPointerPosition();
+      const rect = new Konva.Rect({
+        x: pointerPosition.x,
+        y: pointerPosition.y,
+        width: 0,
+        height: 0,
+        // fill: "blue",
+        stroke: "black",
+        strokeWidth: 2,
+      });
+      layer?.add(rect);
+      setRectangles([...rectangles, rect]);
+    } else if (drawingMode === "circle") {
+      const stage = stageRef.current;
+      const layer = stage.getLayer();
+      const pointerPosition = stage.getPointerPosition();
+      const circle = new Konva.Circle({
+        x: pointerPosition.x,
+        y: pointerPosition.y,
+        radius: 0,
+        // fill: "red",
+        stroke: "black",
+        strokeWidth: 2,
+      });
+      layer?.add(circle);
+      setCircles([...circles, circle]);
+    } else {
+      const stage = stageRef.current;
+      const layer = stage.getLayer();
+      const pointerPosition = stage.getPointerPosition();
+      const line = new Konva.Line({
+        points: [pointerPosition.x, pointerPosition.y],
+        stroke: "black",
+        strokeWidth: 2,
+        lineCap: "round",
+        lineJoin: "round",
+      });
+      layer?.add(line);
+      setLines([...lines, line]);
+    }
   };
 
   const handleMouseMove = event => {
-    console.log("handleMouseMove");
     if (drawing) {
-      const stage = stageRef.current;
-      const layer = stage?.getLayer();
-      const pointerPosition = stage.getPointerPosition();
-      const lastLine = lines[lines.length - 1];
-      lastLine.points(
-        lastLine.points().concat([pointerPosition.x, pointerPosition.y])
-      );
-      layer?.batchDraw();
-      // replace last
-      lines.splice(lines.length - 1, 1, lastLine);
-      setLines(lines.concat());
+      if (drawingMode === "rectangle") {
+        const stage = stageRef.current;
+        const layer = stage?.getLayer();
+        const pointerPosition = stage.getPointerPosition();
+        const lastRect = rectangles[rectangles.length - 1];
+        lastRect.width(pointerPosition.x - lastRect.x());
+        lastRect.height(pointerPosition.y - lastRect.y());
+        layer?.batchDraw();
+        // replace last
+        rectangles.splice(rectangles.length - 1, 1, lastRect);
+        setRectangles(rectangles.concat());
+      } else if (drawingMode === "circle") {
+        const stage = stageRef.current;
+        const layer = stage?.getLayer();
+        const pointerPosition = stage.getPointerPosition();
+        const lastCircle = circles[circles.length - 1];
+        const dx = pointerPosition.x - lastCircle.x();
+        const dy = pointerPosition.y - lastCircle.y();
+        const radius = Math.sqrt(dx * dx + dy * dy);
+        lastCircle.radius(radius);
+        layer?.batchDraw();
+        // replace last
+        circles.splice(circles.length - 1, 1, lastCircle);
+        setCircles(circles.concat());
+      } else {
+        const stage = stageRef.current;
+        const layer = stage?.getLayer();
+        const pointerPosition = stage.getPointerPosition();
+        const lastLine = lines[lines.length - 1];
+        lastLine.points(
+          lastLine.points().concat([pointerPosition.x, pointerPosition.y])
+        );
+        layer?.batchDraw();
+        // replace last
+        lines.splice(lines.length - 1, 1, lastLine);
+        setLines(lines.concat());
+      }
     }
   };
 
@@ -47,26 +105,32 @@ function Whiteboard() {
   };
 
   const handleLine = () => {
-    console.log("handleLine");
-    const stage = stageRef.current;
-    const layer = stage.getLayer();
-    const line = new Konva.Line({
-      points: [100, 100, 200, 200],
-      stroke: "black",
-      strokeWidth: 2,
-      lineCap: "round",
-      lineJoin: "round",
-    });
-    layer?.add(line);
-    setLines([...lines, line]);
+    setDrawingMode("freehand");
+  };
+
+  const handleRectangle = () => {
+    setDrawingMode("rectangle");
+  };
+
+  const handleCircle = () => {
+    setDrawingMode("circle");
   };
 
   const handleSaveDrawing = () => {
     // save logics
   };
 
+  const handleClearDrawing = () => {
+    // save logics
+    setLines([]);
+    setRectangles([]);
+    setCircles([]);
+  };
+
   console.log(drawing);
   console.log(lines);
+  console.log(rectangles);
+  console.log(circles);
 
   return (
     <div>
@@ -94,6 +158,33 @@ function Whiteboard() {
               />
             );
           })}
+          {rectangles.map((rect, index) => {
+            return (
+              <Rect
+                key={index}
+                x={rect.x()}
+                y={rect.y()}
+                width={rect.width()}
+                height={rect.height()}
+                fill={rect.fill()}
+                stroke={rect.stroke()}
+                strokeWidth={rect.strokeWidth()}
+              />
+            );
+          })}
+          {circles.map((circle, index) => {
+            return (
+              <Circle
+                key={index}
+                x={circle.x()}
+                y={circle.y()}
+                radius={circle.radius()}
+                fill={circle.fill()}
+                stroke={circle.stroke()}
+                strokeWidth={circle.strokeWidth()}
+              />
+            );
+          })}
         </Layer>
       </Stage>
       <div className="p-10 flex flex-wrap justify-center items-center">
@@ -102,6 +193,27 @@ function Whiteboard() {
           onClick={handleLine}
         >
           Line
+        </button>
+
+        <button
+          className="px-4 py-2 rounded-md bg-white border border-purple-700 hover:bg-purple-700 hover:text-white hover:border-transparent mx-5 hover:bg-opacity-80 transition-all duration-300 active:scale-[1.97] hover:scale-125"
+          onClick={handleRectangle}
+        >
+          Rectangle
+        </button>
+
+        <button
+          className="px-4 py-2 rounded-md bg-white border border-purple-700 hover:bg-purple-700 hover:text-white hover:border-transparent mx-5 hover:bg-opacity-80 transition-all duration-300 active:scale-[1.97] hover:scale-125"
+          onClick={handleCircle}
+        >
+          Circle
+        </button>
+
+        <button
+          className="px-4 py-2 rounded-md bg-white border border-red-700 hover:bg-red-700 text-red-700 hover:text-white hover:border-transparent mx-5 hover:bg-opacity-80 transition-all duration-300 active:scale-[1.97] hover:scale-125"
+          onClick={handleClearDrawing}
+        >
+          Clear Drawing
         </button>
 
         <button
